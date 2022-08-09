d108527769bba0f3e553dbb57c7e5300a9dca7ca
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-08-09 15:14:25 -0500

    setting up place form/added styling

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,11 +1,48 @@
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, TextInput, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { Colors } from '../../constants/colors';
 
 function PlaceForm() {
+  const [enteredTitle, setEnteredTitle] = useState('');
+
+  // rejister key stroke in this form using state and helper function
+  function changeTitleHandler(enteredText) {
+    setEnteredTitle(enteredText);
+  }
+
   return (
-    <View>
-      <Text>The Place Form</Text>
-    </View>
+    <ScrollView style={styles.form}>
+      <View>
+        <Text style={styles.label}></Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={changeTitleHandler}
+          value={enteredTitle}
+        />
+      </View>
+    </ScrollView>
   );
 }
 
 export default PlaceForm;
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: 'bold',
+    marginBottom: 4,
+    color: Colors.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: Colors.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: Colors.primary100,
+  },
+});
