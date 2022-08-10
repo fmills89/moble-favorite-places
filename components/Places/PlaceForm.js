ebc81aef9f0fc255058937b59384d76ea90758b7
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-08-10 11:01:00 -0500

    rqst camera permiss on ios/android working

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,6 +1,7 @@
 import { View, Text, ScrollView, TextInput, StyleSheet } from 'react-native';
 import { useState } from 'react';
 import { Colors } from '../../constants/colors';
+import ImagePicker from './ImagePicker';
 
 function PlaceForm() {
   const [enteredTitle, setEnteredTitle] = useState('');
@@ -20,6 +21,7 @@ function PlaceForm() {
           value={enteredTitle}
         />
       </View>
+      <ImagePicker />
     </ScrollView>
   );
 }
