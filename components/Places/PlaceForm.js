c4cbd8d8f24dc13d9fd2b475b4983b790e4948a6
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-08-17 12:46:27 -0500

    worked on saving place data

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -4,8 +4,9 @@ import { Colors } from '../../constants/colors';
 import ImagePicker from './ImagePicker';
 import LocationPicker from './LocationPicker';
 import Button from '../ui/Button';
+import { Place } from '../../models/place';
 
-function PlaceForm() {
+function PlaceForm({ onCreatePlace }) {
   const [enteredTitle, setEnteredTitle] = useState('');
   const [selectedImage, setSelectedImage] = useState();
   const [pickedLocation, setPickedLocation] = useState();
@@ -23,7 +24,10 @@ function PlaceForm() {
     setPickedLocation(location);
   }, []);
 
-  function savePlaceHandler() {}
+  function savePlaceHandler() {
+    const placeData = new Place(enteredTitle, selectedImage, pickedLocation);
+    onCreatePlace(placeData);
+  }
 
   return (
     <ScrollView style={styles.form}>
